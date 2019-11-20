d65943b2679bddde4ad7ec0eb22518c707187a68
KhallilB <khallil.bailey@gmail.com>
2019-11-20 11:12:41 -0800

    [add] working on new hero

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// Components
+import Hero from '../hero/Hero';
+
+function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Hero.css'
+
+const Hero = () => (
+    <div className="hero-container">
+        <div className="hero-background" />
+
+        <div className="hero-layer">
+            <h1>Khallil Bailey</h1>
+            <p>
+                “Art and love are the same thing: It’s the process of seeing yourself in things that are not you.”
+                 - Chuck Klosterman
+            </p>
+        </div>
+    </div>
+)
+
+export default Hero;
