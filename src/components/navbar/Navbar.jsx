dc8af8420226f031fbb26a80a176d50004c26368
KhallilB <khallil.bailey@gmail.com>
2019-11-20 17:18:40 -0800

    [add] working navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+import profileImg from '../../assets/images/profile-img.jpg';
 
 const Navbar = () => (
     <header>
-        nav works!
+        <div className="profile-picture">
+            <img src={profileImg} alt="Khallil" />
+        </div>
+
+        <nav className="active">
+            <ul>
+                <li href="#"><Link to="/">Home</Link></li>
+                <li href="#"><Link to="/about">About</Link></li>
+                <li href="#"><Link to="/portfolio">Portfolio</Link></li>
+                <li href="#"><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+
+        <div class="menu">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+        </div>
     </header>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/App.jsx';
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router
+} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import App from './components/app/App.jsx';
+import Navbar from './components/navbar/Navbar.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <Navbar />
+    <Switch>
+      <div>
+        <Route path='/' component={App}></Route>
+      </div>
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
