0777a6b22b125f4fbbf2f5de344274ff22093362
KhallilB <khallil.bailey@gmail.com>
2019-12-02 08:40:27 -0800

    [add]  Tab Menu

--- src/components/home/tabMenu/TabMenu.jsx
+++ src/components/home/tabMenu/TabMenu.jsx
@@ -2,27 +2,43 @@ import React from 'react';
 import './TabMenu.css';
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
 
-const TabMenu = () => (
-    <Tabs
-        activeTab={{
+class TabMenu extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            tabIndex: 0
+        };
+    }
 
-        }}
-    >
-        <TabList>
-            <Tab id="about" title="About Me">About Me</Tab>
-            <Tab id="education" title="Education">Education</Tab>
-            <Tab id="experience" title="Experience">Experience</Tab>
-        </TabList>
+    render() {
+        return (
+            <div className="tabContainer">
+                <Tabs
+                    activeTab={{
+                        id: "about"
+                    }}
+                    selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}
+                >
+                    <TabList>
+                        <Tab id="about" title="About Me">About Me</Tab>
+                        <Tab id="education" title="Education">Education</Tab>
+                        <Tab id="experience" title="Experience">Experience</Tab>
+                    </TabList>
 
-        <TabPanel>
-            <h1>Tab1 Content</h1>
-        </TabPanel>
-        <TabPanel>
-            <h1>Tab2 Conent</h1>
-        </TabPanel>
-    </Tabs>
-)
+                    <TabPanel>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt eget nullam non nisi est. Tristique senectus et netus et malesuada fames. Praesent tristique magna sit amet purus gravida quis. Feugiat in fermentum posuere urna nec tincidunt. Elit duis tristique sollicitudin nibh sit amet commodo.</p>
+                    </TabPanel>
+                    <TabPanel>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mauris commodo quis. Molestie a iaculis at erat pellentesque adipiscing commodo elit. Enim lobortis scelerisque fermentum dui faucibus.</p>
+                    </TabPanel>
+                    <TabPanel>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut diam quam nulla porttitor massa id neque. Non pulvinar neque laoreet suspendisse.</p>
+                    </TabPanel>
+                </Tabs>
+            </div>
+        )
+    }
+}
 
 export default TabMenu;
---
+++ src/components/utils/button/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Button.css';
+
+class Button extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                Button
+            </div>
+        )
+    }
+}
+
+export default Button;
