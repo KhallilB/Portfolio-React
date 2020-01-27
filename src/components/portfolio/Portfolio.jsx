d6699488bc563514490a9405e59bafb5d9430cdd
KhallilB <khallil.bailey@gmail.com>
2020-01-27 13:21:45 -0800

    [add] portfolio styling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Portfolio.scss';
+import './Portfolio.css';
 import Projects from '../../projects.json';
 
 class Portfolio extends React.Component {
@@ -19,27 +19,29 @@ class Portfolio extends React.Component {
         let { projects } = this.state;
         return (
             <div className="portfolio_container">
+                <h1>All Projects</h1>
+                <div className="protfolio_divider" />
                 {projects.map((project, index) => {
                     return (
-                        <div className="project_container">
+                        <div key={index} className="project_container">
                             <div className="project_left">
                                 <img src={project.project_img} alt="#" />
                             </div>
                             <div className="project_right">
                                 <h1>{project.title}</h1>
-                                <hr />
+                                <div className="line" id="project_divider" />
                                 <h4>{project.description}</h4>
                                 <li>
                                     {project.tags.map((tags, index) => {
                                         return (
-                                            <span index={index}>{tags}</span>
+                                            <div className="project_tags" index={index}><p>{tags}</p></div>
                                         )
                                     })}
                                 </li>
 
                                 <div className="link_button_group">
                                     <a href={project.live_link} target="_blank" rel="noopener noreferrer"><button> Live Project</button></a>
-                                    <button>Github</button>
+                                    <a href={project.github_link} target="_blank" rel="noopener noreferrer"><button>Github</button></a>
                                 </div>
                             </div>
                         </div>
