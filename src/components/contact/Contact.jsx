5ff8e8d3b361bef686876eaca1d7dd84874abe2e
KhallilB <khallil.bailey@gmail.com>
2020-01-27 20:25:01 -0800

    [add] Contact component

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Contact.css';
+
+class Contact extends React.Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="contact_container">
+                <h1>Contact</h1>
+            </div>
+        )
+    }
+}
+
+export default Contact;
